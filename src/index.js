15d62114a1900ded8822a87949b2167a9bd36501
AlexStepanchuk <666stepashka666@gmail.com>
2021-01-31 12:25:44 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
+// module.exports = function check(str, bracketsConfig) {
+//   if (str.length <= 1) return false
+
+//   let matchingOpeningBracket, ch
+//   let stack = []
+  
+//   let openingBrackets = bracketsConfig.map( (value) => value[0] )
+//   let closingBrackets = bracketsConfig.map( (value) => value[1] )
+
+//   for (let i = 0; i < str.length; i++) {
+//     ch = str[i]
+
+//     if (closingBrackets.indexOf(ch) > -1) {
+//       matchingOpeningBracket = openingBrackets[closingBrackets.indexOf(ch)]
+//       if (stack.length == 0 || (stack.pop() != matchingOpeningBracket)) {
+//         return false
+//       }
+//     } else {
+//       stack.push(ch)
+//     }
+//   }
+
+//   return (stack.length === 0)
+// }
+
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  for (let i = 0; i <= bracketsConfig.length; i++) {
+    bracketsConfig.forEach(value => {
+        const brackets = value[0] + value[1]
+        while (str.indexOf(brackets) >= 0) {
+          const index = str.indexOf(brackets)
+          str = str.slice(0, index) + str.slice(index + 2)
+        }
+    })
+  }
+  return str ? false : true
 }
